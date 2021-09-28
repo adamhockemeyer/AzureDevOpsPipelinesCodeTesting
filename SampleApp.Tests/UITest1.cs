52614e176bd416f825892d05fe24e42c300b66bd
Adam Hockemeyer <hock2k5@gmail.com>
2021-09-27 21:02:02 -0400

    updates

--- SampleApp.Tests/UITest1.cs
+++ SampleApp.Tests/UITest1.cs
@@ -40,13 +40,6 @@ namespace SampleApp.Tests
             switch (browser)
             {
                 case "Chrome":
-                    ChromeOptions options = new ChromeOptions();
-                    options.AddArguments("start-maximized"); // open Browser in maximized mode
-                    options.AddArguments("disable-infobars"); // disabling infobars
-                    options.AddArguments("--disable-extensions"); // disabling extensions
-                    options.AddArguments("--disable-gpu"); // applicable to windows os only
-                    options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                    options.AddArguments("--no-sandbox"); // Bypass OS security model
                     driver = new ChromeDriver(options);
                     break;
                 case "Firefox":
